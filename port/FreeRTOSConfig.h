149c9ee2d5b785b06ce25c865021b5f88585baf8
ramram885 <rammaheshram1234@gmail.com>
2025-07-26 06:30:07 +0000

    Integrate FreeRTOS kernel source code - first steps

---
+++ port/FreeRTOSConfig.h
@@ -0,0 +1,13 @@
+/*
+* FreeRTOSConfig.h file defines the configurations needed to schedule the
+* tasks like how many tasks are we needed and the priorites for each task
+* (Like software configurations)
+*/
+
+#ifndef _CONFIG_H__     /* Make sure that the configuration is included only once */
+#define _CONFIG_H__
+
+#define configTICK_TYP_WIDTH_IN_BITS    TICK_TYPE_WIDTH_32_BITS
+#define configUSE_16_BIT_TICKS          1
+
+#endif
--- port/start.c
+++ port/start.c
@@ -1,20 +1,7 @@
-/*
-When the ramya() function is called from start(), compiler tries to execute
-the function ramya(). But we will end up with CPU crash. Because to execute
-the another function call we need stack memory. But in our case we have not
-initialized the correct value for stack pointer.
-*/
-void ramya () {
-    return;
-}
+#include "FreeRTOS.h"  // The FreeRtos.h file will include our FreeRTOSConfig.h file into the compilation process.
+#include "task.h"
 
-// Will call this C function from assembly program (boot.s) and let see what is happening
-// To call this function from assembly program in reset_handler part branch this function and look the main.elf.lst file.
-// E.x In boot.S file,
-/* reset_handler:
-   b start
-*/
 void start () {
-    ramya();
+    vTaskStartScheduler();
     return;
 }
