7389ec0b8cf04e4e09372a0b17f28f2390c9ed86
ramram885 <rammaheshram1234@gmail.com>
2025-07-26 11:11:58 +0000

    Create two tasks

--- port/start.c
+++ port/start.c
@@ -1,7 +1,30 @@
 #include "FreeRTOS.h"  // The FreeRtos.h file will include our FreeRTOSConfig.h file into the compilation process.
 #include "task.h"
 
+/*
+* In tasks.c file these functions will be passed as function pointers in xTaskCreate() functions parameters
+* BaseType_t xTaskCreate( TaskFunction_t pxTaskCode,...)
+*/
+static void vTask1(void *pvParameters) {
+    volatile int a = 0;
+    while (1) {
+        a ++;
+    };
+}
+
+static void vTask2(void *pvParameters) {
+    volatile int b = 0;
+    while (1) {
+        b ++;
+    };
+}
 void start () {
+
+    BaseType_t xReturn;
+
+    xReturn = xTaskCreate(vTask1, "T1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xReturn = xTaskCreate(vTask1, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
     vTaskStartScheduler();
     return;
 }
