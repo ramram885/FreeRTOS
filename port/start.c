924a3b05d9911ed5d412c475ee292f39e1b0979d
ramram885 <rammaheshram1234@gmail.com>
2025-07-30 15:21:26 +0000

    Add bootup code and add RAM section in linker script file

--- port/start.c
+++ port/start.c
@@ -18,9 +18,29 @@ static void vTask2(void *pvParameters) {
         b ++;
     };
 }
+
+extern uint32_t _sdata;  // Start of .data in RAM
+extern uint32_t _edata;  // End of .data in RAM
+extern uint32_t _sidata; // Start of .data in FLASH
+extern uint32_t _sbss;   // Start of .bss
+extern uint32_t _ebss;   // End of .bss
+
 void start () {
 
     BaseType_t xReturn;
+
+    // Initialize .data section (Copy from FLASH to RAM)
+    uint32_t  *src = &_sidata; // Source (flash)
+    uint32_t  *dst = &_sdata;  //Destination (RAM)
+    while (dst < &_edata) {
+        *dst++ = *src++;  // Copy word and increment pointers
+    }
+
+    //Initialize .bss section (zero out)
+    dst = &_sbss;
+    while (dst < &_ebss) {
+        *dst++ = 0; // Zero word and increment pointer
+    }
 
     xReturn = xTaskCreate(vTask1, "T1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xReturn = xTaskCreate(vTask1, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
